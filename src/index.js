1b768071b078b5273214d36dd1b40b93490f6e43
Anuja Ulpathakumbura <anuja.ulp@gmail.com>
2021-06-12 22:57:05 +0800

    clean up package.json

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const elvenSheildRecipe = {
+    leatherStripe: 2,
+    itonIngot: 1,
+    refinedMoonstone: 4,
+};
+
+const elevenGauntletsRecipe = {
+    ...elvenSheildRecipe,
+    leather: 1,
+    refinedMoonstone: 5,
+}
+
+console.log(elvenSheildRecipe);
+console.log(elevenGauntletsRecipe);
